aece8389484567ef9c7fa176ca04e5228b7b16e1
kyle <github@kylemiller.com>
2022-11-01 23:52:19 -0500

    fixed contact form submnit, cloenaed up landing page

--- scripts/copyright.js
+++ scripts/copyright.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   // Handler when the DOM is fully loaded
   console.log("loading copyright year event listener..");
-  
+
   document.getElementById('copyright_year').innerHTML = new Date().getFullYear();
 
 });
