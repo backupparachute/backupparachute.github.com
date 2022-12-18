bed7881f156b328aa4d90e4d3480a11822d6ec73
kyle <github@kylemiller.com>
2022-12-18 11:59:03 -0600

    tools nav and tools page links with utm data

--- scripts/tool_nav.js
+++ scripts/tool_nav.js
@@ -1,45 +1,55 @@
 var thisScript = document.currentScript;
-var content = thisScript.getAttribute("data-content");
+var campaign = thisScript.getAttribute("data-campaign");
 
-setInterval(() => console.log(thisScript.src), 2000);
+// setInterval(() => console.log(thisScript.src), 2000);
 
 const nav_html = `<nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="https://backupparachute.com?utm_source=backupparachute&utm_medium=logo&utm_campaign=basic_auth_generator&utm_content=${content}"><img src="https://backupparachute.com/images/bps-logo-green.png" class="" height="40px"></a>
+    <a class="navbar-brand" href="https://backupparachute.com?utm_source=backupparachute&utm_medium=logo&utm_campaign=${campaign}"><img src="https://backupparachute.com/images/bps-logo-green.png" class="" height="40px"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-				<a class="navbar-brand" href="https://backupparachute.com?utm_source=backupparachute&utm_medium=logo&utm_campaign=basic_auth_generator"><img src="https://backupparachute.com/images/bps-logo-green.png" class="" height="60px"></a>
+				<a class="navbar-brand" href="https://backupparachute.com?utm_source=backupparachute&utm_medium=tools_logo&utm_campaign=${campaign}&utm_content=logolink"><img src="https://backupparachute.com/images/bps-logo-green.png" class="" height="60px"></a>
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="https://backupparachute.com?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/contact?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Contact</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/tools?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Tools</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/dev-release-calculator?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Dev Release Calculator</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/basic-auth-generator?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Basic Auth Generator</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/basic-auth-generator?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Base64 Decoder</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/tranche-calculator?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Tranche Calculator</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/sdr-calculator?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">SDR Calculator</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://backupparachute.com/goal-tracker?utm_source=backupparachute&utm_medium=tools_nav&utm_campaign=${campaign}&utm_content=listtextlink">Goal Tracker</a>
           </li>
         </ul>
+        <!--
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
+        -->
       </div>
     </div>
   </div>
@@ -48,7 +58,6 @@ const nav_html = `<nav class="navbar navbar-light bg-light">
 
 document.addEventListener("DOMContentLoaded", function(){
   build_nav();
-  console.log("content = "+content);
 });
 
 function build_nav() {
