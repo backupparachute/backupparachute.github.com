cf00e3bb373e717f735decb8a6885f9e753f5e00
kyle <github@kylemiller.com>
2023-05-05 14:21:05 -0500

    updated add verbiage

--- scripts/ads.js
+++ scripts/ads.js
@@ -7,7 +7,7 @@ var campaign = thisScript.getAttribute("data-campaign");
 const ad_html = `<div class="col text-end">
 			<a href="https://backupparachute.com/contact?utm_source=backupparachute&utm_medium=ad-link&utm_campaign=${campaign}&utm_content=adtextlink">Get a custom app like this for your website today!</a>
 			<div class="">
-			<small><mark>Flat rate pricing, starts at $1000</mark></small>
+			<small><mark>Flat rate pricing!</mark></small>
 			</div>
 		</div>
 `;
